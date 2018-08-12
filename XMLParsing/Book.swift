710f35e0801ba6fc450c8a842f2f1ea816493c41
ㄱ <a402-13@402-13ui-imac.local>
2018-08-12 13:39:25 +0900

    create XML Parsing

---
+++ XMLParsing/Book.swift
@@ -0,0 +1,16 @@
+//
+//  Book.swift
+//  XMLParsing
+//
+//  Created by 402-13 on 2018. 8. 12..
+//  Copyright © 2018년 402-13. All rights reserved.
+//
+
+import UIKit
+
+class Book {
+    var bookId:String?
+    var title:String?
+    var author:String?
+    var summary:String?
+}
